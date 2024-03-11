b2a0e71d22b9b1d0e0ab2edcaf1982d6aeb7cb53
Chutipon Pimsarn <neartop21@gmail.com>
2024-03-11 16:53:15 +0700

    update หน้าจอ map

--- web-app/src/components/MapBurnScar.js
+++ web-app/src/components/MapBurnScar.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {CircleMarker, GeoJSON, Popup} from "react-leaflet";
 import L from "leaflet"; // import Leaflet library
-import axios from 'axios';
+import './custom.css';
 
 
 
 const MapBurnScar = () => {
   const [firmsData, setFirmsData] = useState([]);
-  const [maxPercent, setMaxPercent] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,34 +14,10 @@ const MapBurnScar = () => {
         "http://localhost:3000/process-shapefiles"
       );
       const data = await response.json();
-      let max = data.reduce((max, obj) => Math.max(max, obj.properties.count), -Infinity);
-      console.log("max =", max);
-      // const geometries = data.map(item => item.geometry);
-      setMaxPercent(max)
       setFirmsData(data);
     };
     fetchData();
   }, []);
-
-  const getColor = (fireType) => {
-    switch(fireType) {
-        case 1: //Wildfire':1
-            return "red";
-        case 2://Prescribed Fire
-            return "orange";
-        case 3://Unknown
-            return "yellow";
-        default://Wildland Fire Use
-            return "blue";
-    }
-}
-
-
-const calculatePercentage = (value) => {
-  return (value / maxPercent) * 100;
-}
-
-
 
 const percentToColor = (percent) => {
   const value = percent / 100;
@@ -56,9 +31,6 @@ const percentToColor = (percent) => {
     return hex.length === 1 ? '0' + hex : hex;
   }).join('');
 
-  console.log('value =', value);
-  console.log('hex =', rgbToHex(red, green, blue));
-
   return rgbToHex(red, green, blue); // Only green component changes
 };
 
@@ -67,61 +39,39 @@ const percentToColor = (percent) => {
     // get the color based on the fire type
     const color = percentToColor(feature.properties.frequency);
     // create a circle marker with a fixed pixel radius of 1
-    // return L.circleMarker(latlng, { radius: 1, color: color, fillOpacity: 1 });
     let marker = L.circleMarker(latlng, { radius: 1, color: color, fillOpacity: 1 });
-    // marker.bindPopup("Burnt Frequency = "+ feature.properties.count +" times / :" + calculatePercentage(feature.properties.count)+" %");
-    //create key from latlong
-    // const keyLatLng = feature.geometry.coordinates.join(',');
-    // const marker = <CircleMarker 
-    //                     key={keyLatLng} 
-    //                     center={latlng}
-    //                     radius={1}
-    //                     color={color}
-    //                     fillOpacity={1}
-    //                 />;
-    const years = feature.properties.year;
-    console.log('years', years)
-    let yearString = '';
-    for (let i = 0; i < years.length; i++) {
-      yearString += `<p>Year: ${years[i]}</p>`;
-    }
-    marker.on('click', () => {
-      getReverseGeocodingData(latlng.lat, latlng.lng)
-        .then(address => {
-          marker.bindPopup(`
-                        <div style="font-family: Arial, sans-serif; padding: 10px; border-radius: 5px;">
-                          <h2 style="margin: 0;">Place: ${address.display_name}</h2>
-                          <p style="margin: 5px 0;">Latitude: ${feature.geometry.coordinates[1]}</p>
-                          <p style="margin: 5px 0;">Longitude: ${feature.geometry.coordinates[0]}</p>
-                          <p style="margin: 5px 0;">Percent of burn: ${feature.properties.frequency} % </p>
-                          <p style="margin: 5px 0;">Burnt scar frequency (times): ${feature.properties.count}</p>
-                          ${years.map(year => `<p style="margin: 5px 0;">Year of burn occur: ${year}</p>`).join('')}
 
-                        </div>
-                      `);
-        })
-        .catch(error => {
-          console.error('Error fetching address', error);
-        });
-    });
+    
     
     return marker;
-    // alternatively, you can create a marker with a custom icon
-    // return L.marker(latlng, { icon: L.icon({ iconUrl: "some-image-url", iconSize: [10, 10] }) });
   };
 
-  const getReverseGeocodingData = async (lat, lon) => {
-    const apiKey = '65eddfd7b586a527220428jro7f022f'; // แทนที่ 'your_api_key' ด้วย API key ของคุณ
-    const url = `https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}&api_key=${apiKey}`;
+  // const getReverseGeocodingData = async (lat, lon) => {
+  //   const apiKey = '65eddfd7b586a527220428jro7f022f'; // แทนที่ 'your_api_key' ด้วย API key ของคุณ
+  //   const url = `https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}&api_key=${apiKey}`;
 
-    try {
-      const response = await axios.get(url);
-      return response.data; // ข้อมูลที่อยู่จะอยู่ใน response.data
-    } catch (error) {
-      console.error('Error fetching reverse geocoding data', error);
-    }
+  //   try {
+  //     const response = await axios.get(url);
+  //     return response.data; // ข้อมูลที่อยู่จะอยู่ใน response.data
+  //   } catch (error) {
+  //     console.error('Error fetching reverse geocoding data', error);
+  //   }
+  // };
+
+  const onEachFeature = (feature, layer) => {
+    // create a popup with the feature's properties
+    let popupContent = ` <div style="font-family: Arial, sans-serif; padding: 10px; border-radius: 5px;">
+      <h4 style="text-align: center">Chiang Rai, Thailand</h4>
+      <table>
+        <tr><td><strong>Latitude:</strong></td><td style="text-align:right">${feature.geometry.coordinates[1]}</td></tr>
+        <tr><td><strong>Longitude:</strong></td><td style="text-align:right">${feature.geometry.coordinates[0]}</td></tr>
+        <tr><td><strong>Burnt ratio :</strong></td><td style="background-color:${percentToColor(feature.properties.frequency)};text-align:right;color:#000000;">${feature.properties.frequency} % </td></tr>
+        <tr><td><strong>Burnt frequency (times) :</strong></td><td style="text-align:right">${feature.properties.count}</td></tr>
+        ${feature.properties.year.map(item => `<tr><td><strong>Year of burned :</strong></td> <td style="text-align:right">${item}</td></tr>`).join('')}
+      </table>
+    </div>`;
+    layer.bindPopup(popupContent, { className: 'custom-popup' }); // add a custom class name
   };
-
 
 
   return (
@@ -133,27 +83,12 @@ const percentToColor = (percent) => {
             return (
               // Pass data to layer via props:
               <>
-                <GeoJSON key={index} data={data} pointToLayer={pointToLayer} />
+                <GeoJSON key={index} data={data} pointToLayer={pointToLayer} onEachFeature={onEachFeature} />
               </>
             )
           })
       }
-      {/* {firmsData.map((feature, index) => {
-        
-        // const { latitude, longitude } = feature.geometry.coordinates;
-        const position = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
-        const color = getColor(feature.properties.FIRE_TYPE_);
-        return (
-            <CircleMarker 
-                key={index} 
-                center={position}
-                radius={1}
-                color={color}
-                fillOpacity={1}
-            >
-          </CircleMarker>
-        );
-      })} */}
+      
     </>
   );
 };
